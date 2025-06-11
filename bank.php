f7b8f99501645c7e8c25af08474213eab19b1388
rirism <riris.melissa191@gmail.com>
2025-06-11 16:02:50 +0700

    update view bank list

--- bank.php
+++ bank.php
@@ -1,22 +1,37 @@
 <?php include 'layouts/session.php'; ?>
 <?php include 'layouts/head-main.php'; ?>
+<?php
+include 'layouts/config.php';
+
+// Ambil data dari tabel bank
+$query = "SELECT * FROM bank";
+$listbank = mysqli_query($link, $query);
+$totalBank = mysqli_num_rows($listbank);
+
+$query = "SELECT COUNT(*) as total FROM bank WHERE isdealerutama = 1";
+$bankDU = mysqli_query($link, $query);
+$row = mysqli_fetch_assoc($bankDU);
+$totalDealerUtama = $row['total'];
+?>
+
 <head>
-<title>Bank | Sistem Informasi Pengawasan Treasuri/Market</title>
- <?php include 'layouts/title-meta.php'; ?>
- <?php include 'layouts/head-css.php'; ?>
+	<title>Bank | Sistem Informasi Pengawasan Treasuri/Market</title>
+	<?php include 'layouts/title-meta.php'; ?>
+	<?php include 'layouts/head-css.php'; ?>
 </head>
+
 <body>
-<div id="global-loader" style="display: none;">
+	<div id="global-loader" style="display: none;">
 		<div class="page-loader"></div>
 	</div>
 
-    <div class="main-wrapper">
-    <?php 
-	include 'layouts/topbar.php';
-	include 'layouts/admin-sidebar.php';
-	?>
-<!-- Page Wrapper -->
-<div class="page-wrapper">
+	<div class="main-wrapper">
+		<?php
+		include 'layouts/topbar.php';
+		include 'layouts/admin-sidebar.php';
+		?>
+		<!-- Page Wrapper -->
+		<div class="page-wrapper">
 			<div class="content">
 
 				<!-- Breadcrumb -->
@@ -38,81 +53,78 @@
 					<div class="d-flex my-xl-auto right-content align-items-center flex-wrap ">
 						<div class="me-2 mb-2">
 							<div class="dropdown">
-								<a href="javascript:void(0);" class="dropdown-toggle btn btn-white d-inline-flex align-items-center" data-bs-toggle="dropdown">
+								<a href="javascript:void(0);"
+									class="dropdown-toggle btn btn-white d-inline-flex align-items-center"
+									data-bs-toggle="dropdown">
 									<i class="ti ti-file-export me-1"></i>Unduh
 								</a>
 								<ul class="dropdown-menu  dropdown-menu-end p-3">
 									<li>
-										<a href="javascript:void(0);" class="dropdown-item rounded-1"><i class="ti ti-file-type-pdf me-1"></i>Unduh PDF</a>
+										<a href="javascript:void(0);" class="dropdown-item rounded-1"><i
+												class="ti ti-file-type-pdf me-1"></i>Unduh PDF</a>
 									</li>
 									<li>
-										<a href="javascript:void(0);" class="dropdown-item rounded-1"><i class="ti ti-file-type-xls me-1"></i>Unduh Excel </a>
+										<a href="javascript:void(0);" class="dropdown-item rounded-1"><i
+												class="ti ti-file-type-xls me-1"></i>Unduh Excel </a>
 									</li>
 								</ul>
 							</div>
 						</div>
 						<div class="mb-2">
-							<a href="#" data-bs-toggle="modal" data-bs-target="#add_assets" class="btn btn-primary d-flex align-items-center"><i class="ti ti-circle-plus me-2"></i>Tambah Bank</a>
+							<a href="#" data-bs-toggle="modal" data-bs-target="#add_assets"
+								class="btn btn-primary d-flex align-items-center"><i
+									class="ti ti-circle-plus me-2"></i>Tambah Bank</a>
 						</div>
 						<div class="ms-2 head-icons">
-							<a href="javascript:void(0);" class="" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-original-title="Collapse" id="collapse-header">
+							<a href="javascript:void(0);" class="" data-bs-toggle="tooltip" data-bs-placement="top"
+								data-bs-original-title="Collapse" id="collapse-header">
 								<i class="ti ti-chevrons-up"></i>
 							</a>
 						</div>
 					</div>
 				</div>
 				<!-- /Breadcrumb -->
+				<div class="row">
+
+					<!-- Total Plans -->
+					<div class="col-lg-3 col-md-6 d-flex">
+						<div class="card flex-fill">
+							<div class="card-body d-flex align-items-center justify-content-between">
+								<div class="d-flex align-items-center overflow-hidden">
+									<div>
+										<span class="avatar avatar-lg bg-dark rounded-circle"><i
+												class="ti ti-users"></i></span>
+									</div>
+									<div class="ms-2 overflow-hidden">
+										<p class="fs-12 fw-medium mb-1 text-truncate">Total Bank</p>
+										<h4><?php echo $totalBank; ?></h4>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-3 col-md-6 d-flex">
+						<div class="card flex-fill">
+							<div class="card-body d-flex align-items-center justify-content-between">
+								<div class="d-flex align-items-center overflow-hidden">
+									<div>
+										<span class="avatar avatar-lg bg-dark rounded-circle"><i
+												class="ti ti-users"></i></span>
+									</div>
+									<div class="ms-2 overflow-hidden">
+										<p class="fs-12 fw-medium mb-1 text-truncate">Total Bank DU PUVA</p>
+										<h4><?php echo $totalDealerUtama; ?></h4>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
 
 				<!-- Bank Lists -->
 				<div class="card">
 					<div class="card-header d-flex align-items-center justify-content-between flex-wrap row-gap-3">
-						<h5>Daftar bank</h5>
-						<div class="d-flex my-xl-auto right-content align-items-center flex-wrap row-gap-3">
-							<div class="me-3">
-								<div class="input-icon-end position-relative">
-									<input type="text" class="form-control date-range bookingrange" placeholder="dd/mm/yyyy - dd/mm/yyyy">
-									<span class="input-icon-addon">
-										<i class="ti ti-chevron-down"></i>
-									</span>
-								</div>
-							</div>
-							<div class="dropdown me-3">
-								<a href="javascript:void(0);" class="dropdown-toggle btn btn-white d-inline-flex align-items-center" data-bs-toggle="dropdown">
-									Status
-								</a>
-								<ul class="dropdown-menu  dropdown-menu-end p-3">
-									<li>
-										<a href="javascript:void(0);" class="dropdown-item rounded-1">Aktif</a>
-									</li>
-									<li>
-										<a href="javascript:void(0);" class="dropdown-item rounded-1">Tidak Aktif</a>
-									</li>
-								</ul>
-							</div> 
-							<div class="dropdown">
-								<a href="javascript:void(0);" class="dropdown-toggle btn btn-white d-inline-flex align-items-center" data-bs-toggle="dropdown">
-									Sortir : 7 Hari Terakhir
-								</a>
-								<ul class="dropdown-menu  dropdown-menu-end p-3">
-									<li>
-										<a href="javascript:void(0);" class="dropdown-item rounded-1">Baru Ditambahkan</a>
-									</li>
-									<li>
-										<a href="javascript:void(0);" class="dropdown-item rounded-1">Urutkan Teratas</a>
-									</li>
-									<li>
-										<a href="javascript:void(0);" class="dropdown-item rounded-1">Urutkan Terbawah</a>
-									</li>
-									<li>
-										<a href="javascript:void(0);" class="dropdown-item rounded-1">Sebulan Terkahir</a>
-									</li>
-									<li>
-										<a href="javascript:void(0);" class="dropdown-item rounded-1">7 Hari Terakhir</a>
-									</li>
-								</ul>
-								</ul>
-							</div>
-						</div>
+						<h5>Daftar Bank</h5>
 					</div>
 					<div class="card-body p-0">
 						<div class="custom-datatable-filter table-responsive">
@@ -128,55 +140,47 @@
 										<th>Nama Bank</th>
 										<th>Dealer Utama</th>
 										<th>30 Bank Besar</th>
-										<th>Status</th>
-										<th></th>
+										<th>Action</th>
 									</tr>
 								</thead>
 								<tbody>
-									<tr>
-										<td>
-											<div class="form-check form-check-md">
-												<input class="form-check-input" type="checkbox">
-											</div>
-										</td>
-										<td>008</td>
-										<td>PT Bank Mandiri (Persero) Tbk</td>
-										<td>Ya</td>
-										<td>Ya</td>
-										<td>
-											<span class="badge badge-success d-inline-flex align-items-center badge-xs">
-												<i class="ti ti-point-filled me-1"></i>Active
-											</span>
-										</td>
-										<td>
-											<div class="action-icon d-inline-flex">
-												<a href="#" class="me-2" data-bs-toggle="modal" data-bs-target="#edit_assets"><i class="ti ti-edit"></i></a>
-												<a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#delete_modal"><i class="ti ti-trash"></i></a>
-											</div>
-										</td>
-									</tr>
-									<tr>
-										<td>
-											<div class="form-check form-check-md">
-												<input class="form-check-input" type="checkbox">
-											</div>
-										</td>
-										<td>147</td>
-										<td>PT Bank Muamalat Indonesia Tbk</td>
-										<td>Tidak</td>
-										<td>Ya</td>
-										<td>
-											<span class="badge badge-success d-inline-flex align-items-center badge-xs">
-												<i class="ti ti-point-filled me-1"></i>Active
-											</span>
-										</td>
-										<td>
-											<div class="action-icon d-inline-flex">
-												<a href="#" class="me-2" data-bs-toggle="modal" data-bs-target="#edit_assets"><i class="ti ti-edit"></i></a>
-												<a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#delete_modal"><i class="ti ti-trash"></i></a>
-											</div>
-										</td>
-									</tr>
+									<?php while ($row = mysqli_fetch_assoc($listbank)): ?>
+										<tr>
+											<td>
+												<div class="form-check form-check-md">
+													<input class="form-check-input" type="checkbox">
+												</div>
+											</td>
+											<td><?= htmlspecialchars($row['kode_bank']) ?></td>
+											<td><?= htmlspecialchars($row['nama_bank']) ?></td>
+											<td>
+												<?php if ($row['isdealerutama'] == 1): ?>
+													<span class="badge badge-success d-inline-flex align-items-center badge-xs">
+														Dealer Utama
+													</span>
+												<?php else: ?>
+													<span
+														class="badge badge-secondary d-inline-flex align-items-center badge-xs">
+														Non Dealer Utama
+													</span>
+												<?php endif; ?>
+											</td>
+											<td><?= htmlspecialchars($row['bank30besar'] ?? '-') ?></td>
+											<!-- Jika kolom bank30besar tersedia -->
+											<td>
+												<div class="action-icon d-inline-flex">
+													<a href="#" class="me-2" data-bs-toggle="modal"
+														data-bs-target="#edit_assets">
+														<i class="ti ti-edit"></i>
+													</a>
+													<a href="javascript:void(0);" data-bs-toggle="modal"
+														data-bs-target="#delete_modal">
+														<i class="ti ti-trash"></i>
+													</a>
+												</div>
+											</td>
+										</tr>
+									<?php endwhile; ?>
 								</tbody>
 							</table>
 						</div>
@@ -199,7 +203,8 @@
 				<div class="modal-content">
 					<div class="modal-header">
 						<h4 class="modal-title">Tambah Bank</h4>
-						<button type="button" class="btn-close custom-btn-close" data-bs-dismiss="modal" aria-label="Close">
+						<button type="button" class="btn-close custom-btn-close" data-bs-dismiss="modal"
+							aria-label="Close">
 							<i class="ti ti-x"></i>
 						</button>
 					</div>
@@ -210,13 +215,13 @@
 									<div class="mb-3">
 										<label class="form-label">Kode Bank</label>
 										<input type="text" class="form-control">
-									</div>									
+									</div>
 								</div>
 								<div class="col-md-6">
 									<div class="mb-3">
 										<label class="form-label">Nama Bank</label>
 										<input type="text" class="form-control">
-									</div>									
+									</div>
 								</div>
 								<div class="col-md-12">
 									<div class="mb-3">
@@ -226,8 +231,8 @@
 											<option>Ya</option>
 											<option>Tidak</option>
 										</select>
-									</div>									
-								</div>						
+									</div>
+								</div>
 								<div class="col-md-12">
 									<div class="mb-3 ">
 										<label class="form-label">30 Bank Besar</label>
@@ -266,7 +271,8 @@
 				<div class="modal-content">
 					<div class="modal-header">
 						<h4 class="modal-title">Rubah Bank</h4>
-						<button type="button" class="btn-close custom-btn-close" data-bs-dismiss="modal" aria-label="Close">
+						<button type="button" class="btn-close custom-btn-close" data-bs-dismiss="modal"
+							aria-label="Close">
 							<i class="ti ti-x"></i>
 						</button>
 					</div>
@@ -277,13 +283,13 @@
 									<div class="mb-3">
 										<label class="form-label">Kode Bank</label>
 										<input type="text" class="form-control">
-									</div>									
+									</div>
 								</div>
 								<div class="col-md-6">
 									<div class="mb-3">
 										<label class="form-label">Nama Bank</label>
 										<input type="text" class="form-control">
-									</div>									
+									</div>
 								</div>
 								<div class="col-md-12">
 									<div class="mb-3">
@@ -293,8 +299,8 @@
 											<option>Ya</option>
 											<option>Tidak</option>
 										</select>
-									</div>									
-								</div>						
+									</div>
+								</div>
 								<div class="col-md-12">
 									<div class="mb-3 ">
 										<label class="form-label">30 Bank Besar</label>
@@ -336,7 +342,8 @@
 							<i class="ti ti-trash-x fs-36"></i>
 						</span>
 						<h4 class="mb-1">Konfirmasi Hapus</h4>
-						<p class="mb-3">Kamu akan menghapus bank ini, apabila sudah dihapus tidak akan bisa dikembalikan.</p>
+						<p class="mb-3">Kamu akan menghapus bank ini, apabila sudah dihapus tidak akan bisa
+							dikembalikan.</p>
 						<div class="d-flex justify-content-center">
 							<a href="javascript:void(0);" class="btn btn-light me-3" data-bs-dismiss="modal">Batal</a>
 							<a href="jabatan.php" class="btn btn-danger">Ya, Hapus</a>
@@ -350,9 +357,10 @@
 
 
 
-    </div>
-<!-- end main wrapper-->
-<!-- JAVASCRIPT -->
-<?php include 'layouts/vendor-scripts.php'; ?>
+	</div>
+	<!-- end main wrapper-->
+	<!-- JAVASCRIPT -->
+	<?php include 'layouts/vendor-scripts.php'; ?>
 </body>
+
 </html>
