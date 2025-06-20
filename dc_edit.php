ccb9b554acc6efae1d11929f829e4bb482771833
rirism <riris.melissa191@gmail.com>
2025-06-20 12:15:26 +0700

    update edit dc method

--- dc_edit.php
+++ dc_edit.php
@@ -6,22 +6,12 @@ $dc_status = $_POST['dc_status'];
 $dc_tier = $_POST['dc_tier'];
 $dc_lokasi = $_POST['dc_lokasi'];
 
-// === HANDLE DC === //
-$cekDC = mysqli_query($link, "SELECT * FROM data_center WHERE kode_bank = '$kode_bank'");
-if (mysqli_num_rows($cekDC) > 0) {
-    // UPDATE
-    $queryDC = "
-        UPDATE data_center 
-        SET status = '$dc_status', tier = '$dc_tier', lokasi = '$dc_lokasi' 
-        WHERE kode_bank = '$kode_bank'
-    ";
-} else {
-    // INSERT
-    $queryDC = "
-        INSERT INTO data_center (kode_bank, status, tier, lokasi)
-        VALUES ('$kode_bank', '$dc_status', '$dc_tier', '$dc_lokasi')
-    ";
-}
+$queryDC = "
+    UPDATE data_center 
+    SET status = '$dc_status', tier = '$dc_tier', lokasi = '$dc_lokasi' 
+    WHERE kode_bank = '$kode_bank'
+";
+
 if (!mysqli_query($link, $queryDC)) {
     die("Gagal menyimpan data DC: " . mysqli_error($link));
 }
